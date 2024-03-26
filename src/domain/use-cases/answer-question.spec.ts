026eecf98a036007ed113bacc06f003da04c8b2e
Marcos Lucas <marcosvaldeni@gmail.com>
2024-03-26 20:21:54 +0000

    Dependências externas

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,16 @@
 import { test, expect } from 'vitest';
 import { AnswerQuestionUseCase } from './answer-question';
+import { AnswerRepository } from '../repositories/answers-repository';
+import { Answer } from '../entities/answer';
 
-test('create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase();
+const fakeAnswerRepository: AnswerRepository = {
+  create: async (answer: Answer) => {},
+};
 
-  const answer = answerQuestion.execute({
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+
+  const answer = await answerQuestion.execute({
     content: 'new answer',
     instructorId: '1',
     questionId: '1',
