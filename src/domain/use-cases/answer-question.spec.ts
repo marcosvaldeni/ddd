2c19c18779bf507f8ccf34ae675848c972302973
Marcos Lucas <marcosvaldeni@gmail.com>
2024-04-02 20:34:59 +0100

    Path aliases e Vitest globals

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import { test, expect } from 'vitest';
 import { AnswerQuestionUseCase } from './answer-question';
 import { AnswerRepository } from '../repositories/answers-repository';
 import { Answer } from '../entities/answer';
