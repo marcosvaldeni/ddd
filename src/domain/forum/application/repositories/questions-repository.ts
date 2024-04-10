02112298b34bf09aa04b4be45a4ce4ed18dda5be
Marcos Lucas <marcosvaldeni@gmail.com>
2024-04-10 19:37:08 +0100

    Caso de uso: Deletar pergunta

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,6 +1,8 @@
 import { Question } from '../../enterprise/entities/question';
 
 export interface QuestionRepository {
+  findById(id: string): Promise<Question | null>;
   findBySlug(slug: string): Promise<Question | null>;
   create(answer: Question): Promise<void>;
+  delete(questionRepository: Question): Promise<void>;
 }
