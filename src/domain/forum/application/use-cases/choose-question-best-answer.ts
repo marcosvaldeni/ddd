485a85d30d780e58ca09b82bbf6ffd53f0010a5f
Marcos Lucas <marcosvaldeni@gmail.com>
2024-04-14 12:55:19 +0100

    Caso de uso: Escolher melhor resposta

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -0,0 +1,50 @@
+import { AnswerRepository } from '../repositories/answers-repository';
+import { Question } from '../../enterprise/entities/question';
+import { QuestionRepository } from '../repositories/questions-repository';
+
+interface ChooseQuestionBestAnswerUseCaseRequest {
+  authorId: string;
+  answerId: string;
+}
+
+interface ChooseQuestionBestAnswerUseCaseResponse {
+  question: Question;
+}
+
+export class ChooseQuestionBestAnswerUseCase {
+  constructor(
+    private questionRepository: QuestionRepository,
+    private answerPepository: AnswerRepository,
+  ) {}
+
+  async execute({
+    answerId,
+    authorId,
+  }: ChooseQuestionBestAnswerUseCaseRequest): Promise<ChooseQuestionBestAnswerUseCaseResponse> {
+    const answer = await this.answerPepository.findById(answerId);
+
+    if (!answer) {
+      throw new Error('Answer not found');
+    }
+
+    const question = await this.questionRepository.findById(
+      answer.questionId.toString(),
+    );
+
+    if (!question) {
+      throw new Error('Question not found');
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.');
+    }
+
+    question.bestAnswerId = answer.id;
+
+    await this.questionRepository.save(question);
+
+    return {
+      question,
+    };
+  }
+}
