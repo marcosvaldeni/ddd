78740b5a6a11b53f9ce2729c39392f75f4156cc3
Marcos Lucas <marcosvaldeni@gmail.com>
2024-07-10 20:30:30 +0100

    Mapping relationships

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,11 +1,21 @@
 import { randomUUID } from "node:crypto";
 
+interface AnswerProps {
+  content: string;
+  questionId: string;
+  authorId: string;
+}
+
 export class Answer {
   public id: string
   public content;
+  public authorId: string;
+  public questionId: string;
 
-  constructor(content: string, id?: string) {
+  constructor({ content, authorId, questionId }: AnswerProps, id?: string) {
     this.content = content;
     this.id = id ?? randomUUID()
+    this.authorId = authorId;
+    this.questionId = questionId;
   }
 }
--- src/domain/entities/questions.ts
+++ src/domain/entities/questions.ts
@@ -1,13 +1,21 @@
 import { randomUUID } from "node:crypto";
 
+export interface QuestionProps {
+  title: string;
+  content: string;
+  authorId: string;
+}
+
 export class Question {
   public id: string
-  public title;
-  public content;
+  public title: string;
+  public content: string;
+  public authorId: string;
 
-  constructor(title: string, content: string, id?: string) {
+  constructor({ title, content, authorId }: QuestionProps, id?: string) {
     this.title = title;
     this.content = content;
-    this.id = id ?? randomUUID()
+    this.authorId = authorId;
+    this.id = id ?? randomUUID();
   }
 }
