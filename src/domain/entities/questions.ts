c9f69c24fc391381674e6eb90d5d5e33eab97c7b
Marcos Lucas <marcosvaldeni@gmail.com>
2024-07-12 17:31:04 +0100

    Slug Value Object

--- src/domain/entities/questions.ts
+++ src/domain/entities/questions.ts
@@ -1,21 +1,25 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 export interface QuestionProps {
   title: string;
   content: string;
+  slug: Slug;
   authorId: string;
 }
 
 export class Question {
-  public id: string
+  public id: string;
   public title: string;
+  public slug: Slug;
   public content: string;
   public authorId: string;
 
-  constructor({ title, content, authorId }: QuestionProps, id?: string) {
-    this.title = title;
-    this.content = content;
-    this.authorId = authorId;
+  constructor(pros: QuestionProps, id?: string) {
+    this.title = pros.title;
+    this.content = pros.content;
+    this.slug = pros.slug;
+    this.authorId = pros.authorId;
     this.id = id ?? randomUUID();
   }
 }
