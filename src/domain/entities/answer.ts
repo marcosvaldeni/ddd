77914bbd68d495fef11f8f87c3d6fa7d02a0874a
Marcos Lucas <marcosvaldeni@gmail.com>
2024-07-15 19:36:50 +0100

    Mapping properties

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,16 @@
 import { Entity } from "../../core/entities/entities";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface AnswerProps {
-  content: string;
-  questionId: string;
   authorId: string;
+  questionId: UniqueEntityID;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-export class Answer extends Entity<AnswerProps> {}
+export class Answer extends Entity<AnswerProps> {
+  get content() {
+    return this.props.content;
+  }
+}
