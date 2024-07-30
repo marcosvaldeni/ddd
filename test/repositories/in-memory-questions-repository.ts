0af35363f35465337ec6fb790dae26ce5e09749a
Marcos Lucas <marcosvaldeni@gmail.com>
2024-07-30 19:36:01 +0100

    Use Case: Edit Question

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -22,6 +22,12 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return question
   }
 
+  async save(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items[itemIndex] = question
+  }
+
   async create(question: Question) {
     this.items.push(question)
   }
