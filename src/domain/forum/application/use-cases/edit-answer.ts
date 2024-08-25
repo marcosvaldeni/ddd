4a5558c484de8b07f14ff09a6a8c8bef88dea309
Marcos Lucas <marcosvaldeni@gmail.com>
2024-08-25 10:25:20 +0100

    Editing question with attachments

--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -1,46 +1,74 @@
 import { Either, left, right } from '@/core/either'
-import { Answer } from '../../enterprise/entities/answer'
-import { AnswersRepository } from '../repositories/answers-repository'
+import { NotAllowedError } from '@/domain/forum/application/use-cases/errors/not-allowed-error'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
+import { QuestionAttachmentList } from '@/domain/forum/enterprise/entities/question-attachment-list'
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { ResourceNotFoundError } from './errors/resource-not-foutnd-error'
-import { NotAllowedError } from './errors/not-allowed-error'
 
-interface EditAnswerUseCaseRequest {
+interface EditQuestionUseCaseRequest {
   authorId: string
-  answerId: string
+  questionId: string
+  title: string
   content: string
+  attachmentsIds: string[]
 }
 
-type EditAnswerUseCaseResponse = Either<
+type EditQuestionUseCaseResponse = Either<
   ResourceNotFoundError | NotAllowedError,
   {
-    answer: Answer
+    question: Question
   }
 >
 
-export class EditAnswerUseCase {
-  constructor(private answersRepository: AnswersRepository) {}
+export class EditQuestionUseCase {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionAttachmentsRepository: QuestionAttachmentsRepository,
+  ) {}
 
   async execute({
     authorId,
-    answerId,
+    questionId,
+    title,
     content,
-  }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
-    const answer = await this.answersRepository.findById(answerId)
+    attachmentsIds,
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
 
-    if (!answer) {
+    if (!question) {
       return left(new ResourceNotFoundError())
     }
-
-    if (authorId !== answer.authorId.toString()) {
+    if (authorId !== question.authorId.toString()) {
       return left(new NotAllowedError())
     }
 
-    answer.content = content
+    const currentQuestionAttachments =
+      await this.questionAttachmentsRepository.findManyByQuestionId(questionId)
 
-    await this.answersRepository.save(answer)
+    const questionAttachmentList = new QuestionAttachmentList(
+      currentQuestionAttachments,
+    )
+
+    const questionAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      })
+    })
+
+    questionAttachmentList.update(questionAttachments)
+
+    question.title = title
+    question.content = content
+    question.attachments = questionAttachmentList
+
+    await this.questionsRepository.save(question)
 
     return right({
-      answer,
+      question,
     })
   }
 }
